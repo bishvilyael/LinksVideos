af41512289ffd9fad80a012566b5a5b0f9888619
bishvilyael <bishvil.yael@gmail.com>
2026-05-03 15:13:16 +0300

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,46 @@
+const videoGroups = [
+  {
+    title: "סרטונים חדשיים תצוגת מחשב",
+    videos: [
+      {
+        title: "יום הולדת 23",
+        id: "YOUTUBE_VIDEO_ID"
+      },
+      {
+        title: "פברואר 2026",
+        id: "YOUTUBE_VIDEO_ID"
+      }
+      {
+        title: "מרץ 2026",
+        id: "YOUTUBE_VIDEO_ID"
+      },
+      {
+        title: "אפריל 2026",
+        id: "YOUTUBE_VIDEO_ID"
+      }
+    ]
+  },
+  {
+    title: "סרטונים חדשיים תצוגת נייד",
+    videos: [
+       {
+        title: "יום הולדת 23",
+        id: "YOUTUBE_VIDEO_ID"
+      },
+      {
+        title: "פברואר 2026",
+        id: "YOUTUBE_VIDEO_ID"
+      }
+      {
+        title: "מרץ 2026",
+        id: "YOUTUBE_VIDEO_ID"
+      },
+      {
+        title: "אפריל 2026",
+        id: "YOUTUBE_VIDEO_ID"
+      }
+    ]
+  },
+  {    ]
+  }
+];
