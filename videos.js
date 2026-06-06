f885ffaa9771edf2e88688831d4b9d883e76b934
MaayanChr <maayan.chr@gmail.com>
2026-06-06 22:24:08 +0300

    Update videos list

--- videos.js
+++ videos.js
@@ -6,7 +6,8 @@ const videoGroups = [
       { title: "פברואר 2026", id: "jkn-JX43Eh0" },
       { title: "מרץ 2026", id: "l43j92i5e1o" },
       { title: "יום הזכרון 2026", id: "LkW4VYv7ow8" },
-      { title: "אפריל 2026", id: "fh5qS6b1L6c" }
+      { title: "אפריל 2026", id: "fh5qS6b1L6c" },
+      { title: "מאי 2026", id: "mXR55we4D2I" }
     ]
   },
   {
@@ -16,7 +17,8 @@ const videoGroups = [
       { title: "פברואר 2026", id: "WhjTJIt3s1A" },
       { title: "מרץ 2026", id: "a4EFKI9WJUw" },
       { title: "יום הזכרון 2026", id: "tRGOq46xfds" },
-      { title: "אפריל 2026", id: "fh5qS6b1L6c" }
+      { title: "אפריל 2026", id: "fh5qS6b1L6c" },
+      { title: "מאי 2026", id: "EQWBD8VJ8Iw" }
     ]
   }
 ];
