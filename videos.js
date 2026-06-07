cf882a754defc1a38005874036bc8f56046eb027
MaayanChr <maayan.chr@gmail.com>
2026-06-07 11:40:11 +0300

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,24 @@
+const videoGroups = [
+  {
+    type: "desktop",
+    videos: [
+      { title: "יום הולדת 23", id: "NJvRGkxUY_I" },
+      { title: "פברואר 2026", id: "jkn-JX43Eh0" },
+      { title: "מרץ 2026", id: "l43j92i5e1o" },
+      { title: "יום הזכרון 2026", id: "LkW4VYv7ow8" },
+      { title: "אפריל 2026", id: "fh5qS6b1L6c" },
+      { title: "מאי - 2026", id: "mXR55we4D2I" }
+    ]
+  },
+  {
+    type: "mobile",
+    videos: [
+      { title: "יום הולדת 23", id: "yTXEEGXGbPE" },
+      { title: "פברואר 2026", id: "WhjTJIt3s1A" },
+      { title: "מרץ 2026", id: "a4EFKI9WJUw" },
+      { title: "יום הזכרון 2026", id: "tRGOq46xfds" },
+      { title: "אפריל 2026", id: "fh5qS6b1L6c" },
+      { title: "מאי 2026", id: "EQWBD8VJ8Iw" }
+    ]
+  }
+];
